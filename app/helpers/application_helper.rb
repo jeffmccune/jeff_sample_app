0d41f9e264feb836345ee2e8378ed35a675bac89
Jeff McCune <jeff@puppetlabs.com>
2014-05-05 11:16:52 -0700

    Test ApplicationHelper#full_title mixin
    Without this patch the full_title method that's mixed into controllers
    is not explicitly tested.  This is a problem because we need to
    re-implement this method in spec/support/utilities
    
    This patch addresses the problem by explicitly adding coverage for the
    helper method and updating the support utilities to simply include the
    application helper class.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def app_name
+    "sample app"
+  end
+
   # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
