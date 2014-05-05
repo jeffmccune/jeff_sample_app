db5539a43dbf9073cfaa2e87e24b59b09516189a
Jeff McCune <jeff@puppetlabs.com>
2014-05-05 10:41:48 -0700

    Add user controller and /signup route
    End of named routes section at:
    http://www.railstutorial.org/book/_single-page#code-home_page_signup_link

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,8 @@ describe "Static pages" do
     it { should have_content('Sample App') }
     it { should have_title("#{app_title}") }
     it { should have_title('| Home') }
+    it { should have_link('Sign up now!', href: signup_path) }
+    it { should have_link(app_name, href: root_path) }
   end
 
   describe "Help page" do
@@ -17,6 +19,7 @@ describe "Static pages" do
 
     it { should have_content('Help') }
     it { should have_title("#{app_title} | Help") }
+    it { should have_link(app_name, href: root_path) }
   end
 
   describe "About page" do
@@ -24,6 +27,7 @@ describe "Static pages" do
 
     it { should have_content('About Us') }
     it { should have_title("#{app_title} | About") }
+    it { should have_link(app_name, href: root_path) }
   end
 
   describe "Contact page" do
@@ -31,5 +35,6 @@ describe "Static pages" do
 
     it { should have_content('Contact Us') }
     it { should have_title("#{app_title} | Contact") }
+    it { should have_link(app_name, href: root_path) }
   end
 end
