890dd62959a596a4362c123ebe3ea7c97dcaf5e5
Jeff McCune <jeff@puppetlabs.com>
2014-05-05 11:01:45 -0700

    Add shared examples for static pages (Ch 5 exercise 2)
    Without this patch there's some duplication of behavior examples in the
    spec tests.  This patch addresses the problem by using shared examples
    to DRY up the examples.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,46 @@ require 'spec_helper'
 
 describe "Static pages" do
   let(:app_title) { "Ruby on Rails Tutorial Sample App" }
+
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+    it { should have_link(app_name, href: root_path) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { "Sample App" }
+    let(:page_title) { 'Home' }
 
-    it { should have_content('Sample App') }
-    it { should have_title("#{app_title}") }
-    it { should have_title('| Home') }
+    it_should_behave_like "all static pages"
+
     it { should have_link('Sign up now!', href: signup_path) }
-    it { should have_link(app_name, href: root_path) }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading) { "Help" }
+    let(:page_title) { "Help" }
 
-    it { should have_content('Help') }
-    it { should have_title("#{app_title} | Help") }
-    it { should have_link(app_name, href: root_path) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { "About Us" }
+    let(:page_title) { "About" }
 
-    it { should have_content('About Us') }
-    it { should have_title("#{app_title} | About") }
-    it { should have_link(app_name, href: root_path) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) { "Contact Us" }
+    let(:page_title) { "Contact" }
 
-    it { should have_selector('h1', text: 'Contact') }
-    it { should have_title("#{app_title} | Contact") }
-    it { should have_link(app_name, href: root_path) }
+    it_should_behave_like "all static pages"
   end
 end
