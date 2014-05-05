9ac49210104d2ba8b42b4a763b8236ac1c43eacb
Jeff McCune <jeff@puppetlabs.com>
2014-05-04 22:41:47 -0700

    Add pretty named routes and links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,17 +9,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the Title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{app_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
@@ -27,12 +27,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{app_title} | Help")
     end
   end
@@ -40,24 +40,24 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{app_title} | About")
     end
   end
 
   describe "Contact page" do
     it "has the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact Us')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{app_title} | Contact")
     end
   end
