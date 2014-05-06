63cd921588b92e4029343ddc06ceef8732e012b4
Jeff McCune <jeff@puppetlabs.com>
2014-05-06 12:18:22 -0700

    Implement sign in failure (8.1)
    This patch implements the behaviors in Section 8.1, namely handling sign
    in failures and error messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      # Create an error message and re-render the signin form
+      # flash.now is specifically designed to be used with render since render
+      # does not count as a visit
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
