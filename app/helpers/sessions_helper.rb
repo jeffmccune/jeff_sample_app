c1b875ae751ff2ea0eccdcfed5fa11f4f2ad7a24
Jeff McCune <jeff@puppetlabs.com>
2014-05-06 14:13:06 -0700

    Add sign_in and current_user helpers (8.2.3)
    Without this patch we don't have a way to track the currently signed in
    user.  This patch addresses the problem by implementing two methods as
    helpers for the Controllers and Views.  sign_in will compare a
    remember_token permanently stored in the session with the hashed token
    in the database and set current_user accordingly.
    
    In addition, the current_user helper will return an instance of the User
    model if there is a currently signed in user.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+
+    # use update_attribute to bypass validations b/c we don't have the password
+    # confirmation.
+    user.update_attribute(:remember_token, User.digest(remember_token))
+    # Set the current user in case use sign_in without a subsequent redirect
+    self.current_user = user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    hashed_remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: hashed_remember_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -104,6 +104,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
