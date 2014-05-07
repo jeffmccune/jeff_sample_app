106c20bec48d89ae5b427ea79d17fc98e6a53a09
Jeff McCune <jeff@puppetlabs.com>
2014-05-07 12:05:34 -0700

    (maint) Reformat method order for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   def index
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -18,10 +22,6 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   def edit
