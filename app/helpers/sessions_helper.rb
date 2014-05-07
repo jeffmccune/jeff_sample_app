35cabc8605d4dc27463edd5b64eb56b3b4d32e6a
Jeff McCune <jeff@puppetlabs.com>
2014-05-06 17:10:20 -0700

    Require the right user (9.2.2)
    Without this patch any user logged into the system can edit and update
    any other user.  This is a problem because users should only be able to
    update their own data.
    
    This patch addresses the problem by ensuring the current user us the
    user being updated.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,4 +30,8 @@ module SessionsHelper
     hashed_remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: hashed_remember_token)
   end
+
+  def current_user?(user)
+    user == current_user
+  end
 end
